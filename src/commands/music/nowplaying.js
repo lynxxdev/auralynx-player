ef7745556628ea8bf014515038720dea2ca09bf4
Freeman <trulyn300@gmail.com>
2025-12-02 23:00:23 -0300

    Atualizar o nowplaying.js

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -6,22 +6,22 @@ module.exports = {
   execute(message) {
     try {
       const player = message.client.manager.queue.get(message.guild.id);
-      if (!player || !player?.current) return message.reply({ embeds: [embedMessage("There is nothing playing in this server.")], ephemeral: true });
+      if (!player || !player?.current) return message.reply({ embeds: [embedMessage("Não há nada tocando nesse servidor.")], ephemeral: true });
 
       const song = player.current.info;
       const seek = player.player.position / 1000;
       const left = song.length / 1000 - seek;
 
       let nowPlaying = embedMessage()
-        .addFields({ name: "Track:", value: `[\`${song.title}\`](${song.uri})`, inline: true })
-        .addFields({ name: "Requested By:", value: `<@${song.requester.id}>`, inline: true })
+        .addFields({ name: "Faixa:", value: `[\`${song.title}\`](${song.uri})`, inline: true })
+        .addFields({ name: "Escolhido (a) por:", value: `<@${song.requester.id}>`, inline: true })
 
       if (song.length > 0 && player.player.position > 1000) {
         nowPlaying
           .addFields({
             name: "\u200b", value: `\`${new Date(seek * 1000).toISOString().substr(11, 8)}` +
               createBar(song.isStream === true ? seek : song.length / 1000, seek, 25)[0] +
-              (song.isStream === true ? " ◉ LIVE" :
+              (song.isStream === true ? " ◉ AO VIVO" :
                 ` ${new Date(song.length).toISOString().substr(11, 8)}\``),
             inline: false
           })
