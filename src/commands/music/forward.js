c872850ff51d4f827b932c056e1884580e17e932
Freeman <trulyn300@gmail.com>
2025-12-02 21:10:10 -0300

    Atualizar o forward.js

--- src/commands/music/forward.js
+++ src/commands/music/forward.js
@@ -7,14 +7,14 @@ module.exports = {
         if (!(await canModifyQueue(message))) return;
 
         const player = message.client.manager.queue.get(message.guild.id);
-        if (player.current.info.isStream) return message.reply({ embeds: [embedMessage("I can't seek live streams.")], ephemeral: true });
+        if (player.current.info.isStream) return message.reply({ embeds: [embedMessage("Não consigo procurar transmissões ao vivo.")], ephemeral: true });
 
         const songtime = player.current.info.length / 1000;
         let seektime = player.player.position / 1000 + Number(message.options.get("seconds").value);
 
-        if (songtime < seektime) return message.reply({ embeds: [embedMessage("Please enter a valid seekpoint in seconds.")], ephemeral: true });
+        if (songtime < seektime) return message.reply({ embeds: [embedMessage("Opa! Insira um ponto de busca válido em segundos.")], ephemeral: true });
 
         await player.player.seekTo(seektime * 1000);
-        return message.reply({ embeds: [embedMessage(`<@${message.author.id}> **Forwarded** the track.`)] })
+        return message.reply({ embeds: [embedMessage(`O <@${message.author.id}> **avançou para a próxima faixa.**`)] })
     },
 };
