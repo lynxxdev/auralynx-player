6234b51c4bbd256ebebbcbfd82a8a64a2e16073f
Freeman <trulyn300@gmail.com>
2025-12-02 23:52:13 -0300

    Atualizar o duplicates.js

--- src/commands/music/duplicates.js
+++ src/commands/music/duplicates.js
@@ -8,15 +8,20 @@ module.exports = {
             if (!(await canModifyQueue(message))) return;
             const player = message.client.manager.queue.get(message.guild.id);
 
-            let count = 0, a = [];;
+            let count = 0;
+            const a = [];
+
             for (let i = 0; i < player.queue.length; i++) {
-                if (a.includes(player.queue[i]?.info.uri)) {
+                const track = player.queue[i];
+                if (a.includes(track?.info.uri)) {
                     player.queue.splice(i--, 1);
                     count++;
+                } else {
+                    a.push(track?.info.uri);
                 }
             }
 
-            return message.reply({ embeds: [embedMessage(`${message.member.user} Removidas **${count}** faixas duplicadas da fila`)] });
+            return message.reply({ embeds: [embedMessage(`${message.member.user} - Removidas **${count}** faixas duplicadas da fila`)] });
         } catch (err) {
             return message.reply({ embeds: [embedMessage("Opa! Ocorreu um erro ao remover os duplicados, por favor tente novamente.")] });
         }
